b02156aa919b9d49b60d124bfe446f68dd703fd1
Pavel Polechtchouk <pavel.overseas@hotmail.com>
2011-01-21 11:57:55 +0100

    starting project

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject textil "1.0.0-SNAPSHOT"
+  :description "TEXTIL is a wiki markup to xHTML converter, as well as a collection of supporting text utilities."
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]]
+
+
+)
---
+++ test/textil/test/core.clj
@@ -0,0 +1,6 @@
+(ns textil.test.core
+  (:use [textil.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
